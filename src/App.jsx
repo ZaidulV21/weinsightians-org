ee7d130f3073dc7ecffbda26f796ac91450fd5f3
Akshat Jaiswal <akshatjaiswal4841@gmail.com>
2024-12-12 18:23:31 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Page1 from "./Page/Page1"
+
+
+export default function App() {
+  return (
+    <div>
+      <Page1/>
+    </div>
+  )
+}
---
+++ src/Page/Page1.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Page1 = () => {
+  return (
+    <div>
+        <div className="h-screen bg-white ">
+
+        </div>
+    </div>
+  )
+}
+
+export default Page1
