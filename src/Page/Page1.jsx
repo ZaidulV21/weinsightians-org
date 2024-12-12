d4e2833b4c49ebf6e5ebcdfb1c536a271f4699d5
Abhishek Kushwaha <abhishekkushwaha4732@gmail.com>
2024-12-12 19:33:18 +0530

    Co-authored-by: Akshat Jaiswal <akshatjaiswal4841@gmail.com>

--- src/Page/Page1.jsx
+++ src/Page/Page1.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import Navbar from '../components/Navbar.jsx'
 const Page1 = () => {
   return (
     <div>
         <div className="h-screen bg-white ">
-
+        <Navbar/>
         </div>
     </div>
   )
