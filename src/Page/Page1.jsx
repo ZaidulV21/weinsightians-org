d45c4d4c2115db44603f8dc2b128b5892fca0a76
Abhishek Kushwaha <abhishekkushwaha4732@gmail.com>
2024-12-13 21:15:45 +0530

    Update Navbar

--- src/Page/Page1.jsx
+++ src/Page/Page1.jsx
@@ -2,12 +2,9 @@ import React from 'react'
 import Navbar from '../components/Navbar.jsx'
 const Page1 = () => {
   return (
-    <div>
         <div className=" bg-custom relative text-black px-16 h-screen  ">
         <Navbar/>
-
-        </div>
-    </div>
+       </div>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,34 +3,42 @@ import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div id='glass-morph' className="flex items-center justify-between font-medium p-5 w-full h-10 bg-green-300 rounded-lg">
-      <Link to='/'><img className='w-10' src='/bars-svgrepo-com.svg' alt="logo" /></Link>
+    <div id='glass-morph' className="flex items-center justify-between  font-medium overflow-hidden p-5 w-full h-20 bg-green-300 rounded-lg">
+      <Link to='/'>
+        <div className='flex items-center gap-2 font-medium'>
+          <img className='w-12 ' src='../../public/bgWIcon.png' alt="logo" />
+          <p>We Insightians</p>
+        </div>
+      </Link>
 
-      <div className='hidden sm:flex gap-5 text-sm text-gray-700'>
-        <NavLink to='/products' className='flex flex-col items-center gap-1'>
+      <div className='hidden sm:flex gap-5 text-sm '>
+        <NavLink to='/products' className='flex flex-col items-center gap-1 hover:text-gray-700'>
           <p>Products</p>
           <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
         </NavLink>
 
-        <NavLink to='/pricing' className='flex flex-col items-center gap-1'>
+        <NavLink to='/pricing' className='flex flex-col items-center gap-1 hover:text-gray-700'>
           <p>Pricing</p>
           <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
         </NavLink>
 
-        <NavLink to='/api' className='flex flex-col items-center gap-1'>
+        <NavLink to='/api' className='flex flex-col items-center gap-1 hover:text-gray-700'>
           <p>Api for Developer</p>
           <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
         </NavLink>
 
-        <NavLink to='/blog' className='flex flex-col items-center gap-1'>
+        <NavLink to='/blog' className='flex flex-col items-center gap-1 hover:text-gray-700'>
           <p>Blog</p>
           <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
         </NavLink>
 
-        <NavLink to='/contact' className='flex flex-col items-center gap-1'>
+        <NavLink to='/contact' className='flex flex-col items-center gap-1 hover:text-gray-700'>
           <p>Contact</p>
           <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
         </NavLink>
+      </div>
+      <div className='flex items-center gap-6'>
+        <button className='px-8 py-3 text-[#fff] rounded-full bg-[#231746] hover:bg-[#534277]'>Call Us</button>
       </div>
     </div>
   )
