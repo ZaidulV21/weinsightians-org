69bb3cf4c43a36fe36979eace13b80fe6dbca7ab
Abhishek Kushwaha <abhishekkushwaha4732@gmail.com>
2024-12-15 00:53:16 +0530

    Update page1

--- src/Page/Page1.jsx
+++ src/Page/Page1.jsx
@@ -2,10 +2,24 @@ import React from 'react'
 import Navbar from '../components/Navbar.jsx'
 const Page1 = () => {
   return (
-        <div className=" bg-custom relative text-black px-16 h-screen  ">
-        <Navbar/>
-       </div>
-  )
+    <div className=" bg-custom text-black px-16 h-screen p-5">
+      <Navbar />
+      <div className='uppercase text-6xl mt-16 font-semibold '>
+        <h1>bring all your</h1>
+        <h1>ideas to life with our</h1>
+        <h1>creative magic</h1>
+      </div>
+      <div className='mt-6'>
+      <p>Our creative team gets to work, crafting a custom design that's not</p>
+      <p>only beautiful but functional. We develop your landing page using</p>
+      <p>the latest technologies and best practices.</p>
+      </div>
+      <div className='flex '>
+        <button className='px-10 py-2 text-[#231746] font-thin rounded-full border-2 bg-[#fff] hover:bg-[#534277] '>Get Started</button>
+        <button disabled="disabled"></button>
+      </div>
+    </div>
+  ) 
 }
 
 export default Page1
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,14 +3,15 @@ import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div id='glass-morph' className="flex items-center justify-between  font-medium overflow-hidden p-5 w-full h-20 bg-green-300 rounded-lg">
+    // Logo 
+    <div id='glass-morph' className="flex items-center justify-between font-medium overflow-hidden p-5 w-full h-20 bg-green-300 rounded-lg">
       <Link to='/'>
         <div className='flex items-center gap-2 font-medium'>
           <img className='w-12 ' src='../../public/bgWIcon.png' alt="logo" />
-          <p>We Insightians</p>
+          <p>WeInsightians</p>
         </div>
       </Link>
-
+{/* Pages  */}
       <div className='hidden sm:flex gap-5 text-sm '>
         <NavLink to='/products' className='flex flex-col items-center gap-1 hover:text-gray-700'>
           <p>Products</p>
@@ -37,8 +38,9 @@ const Navbar = () => {
           <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
         </NavLink>
       </div>
+{/* Button  */}
       <div className='flex items-center gap-6'>
-        <button className='px-8 py-3 text-[#fff] rounded-full bg-[#231746] hover:bg-[#534277]'>Call Us</button>
+        <button className='px-8 py-3 text-[#fff] rounded-full border-2 bg-[#231746] hover:bg-[#534277] '>Call Us</button>
       </div>
     </div>
   )
