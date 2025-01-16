221f207a20e7237e89f9adc01793cf9f7a0e00ca
Akshat Jaiswal <akshatjaiswal4841@gmail.com>
2025-01-16 12:49:54 +0530

    Complete Service Route responsiveness

--- src/components/OurServices2.jsx
+++ src/components/OurServices2.jsx
@@ -16,7 +16,7 @@ const OurServices2 = () => {
     {
       title: "Social Media Management",
       imgSrc: "/img/ser_Social.png",
-      description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
+      description: "It  a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
     },
     {
       title: "Animation",
@@ -26,15 +26,15 @@ const OurServices2 = () => {
   ];
 
    return services.map((service) => (
-    <div key={service.title} className='flex h-[40%] mt-10 gap-5 relative'>
-      <div className='h-full w-full relative'>
-        <h1 className='absolute bottom-5 font-bold text-6xl font-[larken]'>{service.title}</h1>
+    <div key={service.title} className='flex flex-col md:flex-row h-auto md:h-[40%] mt-20 gap-5 relative'>
+      <div className='w-full md:w-1/3 relative'>
+        <h1 className='absolute bottom-5 font-bold text-4xl md:text-6xl font-[larken]'>{service.title}</h1>
       </div>
-      <div className='flex items-center h-full w-full'>
-        <img className="rounded-xl h-80 transition-all duration-500 hover:scale-105" src={service.imgSrc} alt={service.title} />
+      <div className='w-full md:w-1/3 flex items-center'>
+        <img className="rounded-xl h-60 md:h-80 transition-all duration-500 hover:scale-105 object-contain" src={service.imgSrc} alt={service.title} />
       </div>
-      <div className='h-full w-full flex items-center font-[gilroy] font-bold text-zinc-800 bg-orange-00'>
-        <h1>{service.description}</h1>
+      <div className='w-full md:w-1/3 flex items-center font-[gilroy] font-bold text-zinc-800 bg-orange-00 overflow-hidden'>
+        <h1 className='text-lg md:text-xl'>{service.description}</h1>
       </div>
       {/* <div className='border-t-2 border-zinc-400'></div> */}
     </div>
--- src/components/ServicePage1.jsx
+++ src/components/ServicePage1.jsx
@@ -8,27 +8,27 @@ const ServicePage1 = () => {
   return (
     <>
       {/* section1 */}
-      <div className='h-screen px-16 p-5'>
+      <div className='h-screen px-4 md:px-16 p-5'>
         <Navbar />
-        <div className='absolute top-36'><h1 className='text-6xl font-medium font-[larken]'>Shaping the best brands <br /> and websites of <br /><span className='text-zinc-500'>tommorrow's greatest players</span></h1></div>
-        <div className='absolute bottom-10 flex justify-between w-[90%]'>
-          <h1 className='font-[gilroy] text-base font-semibold' >WeInsightians is here for the ones who don't just follow trends, they set <br />them. Our services go beyond design, we're talking premium, high-impact <br />branding and web experiences that make your brand unforgettable.</h1>
-          <img className='rotate36 h-28' src="/img/64bbbf416decd23360ebb88c_get-in-touch-badge.svg" alt="getintotouch" />
+        <div className='absolute top-36'><h1 className='text-4xl md:text-6xl font-medium font-[larken]'>Shaping the best brands <br /> and websites of <br /><span className='text-zinc-500'>tommorrow's greatest players</span></h1></div>
+        <div className='absolute bottom-10 flex justify-between w-full md:w-[90%]'>
+          <h1 className='font-[gilroy] text-sm md:text-base font-semibold' >WeInsightians is here for the ones who don't just follow trends, they set <br />them. Our services go beyond design, we're talking premium, high-impact <br />branding and web experiences that make your brand unforgettable.</h1>
+          <img className='hidden md:inline h-20 rotate36 md:h-28' src="/img/64bbbf416decd23360ebb88c_get-in-touch-badge.svg" alt="getintotouch" />
         </div>
       </div>
       {/* section2 */}
-      <div className='h-full w-full px-16 relative'>
-        <div className='flex mt-28 justify-between items-center font-[gilroy]'>
+      <div className='h-full w-full px-4 md:px-16 relative'>
+        <div className='flex mt-20 md:mt-28 justify-between items-center font-[gilroy]'>
           <div className='relative'>
             <h1 className='mb-5 text-zinc-500'><span className='font-black'>•</span> Create gibberish history with us ewfuheif eufhiefewi </h1>
-            <h1 className=' text-5xl'>From concept to execution our  <br />services are designed to set you apart</h1>
+            <h1 className='text-4xl md:text-5xl'>From concept to execution our  <br />services are designed to set you apart</h1>
           </div>
           <div className='relative'>
             <h1 className=''>Start your project with us today</h1>
-            <div className='absolute left-0 w-20 h-[2px] bg-zinc-500 transition-all duration-500 hover:w-full'></div>
+            <div className='absolute left-0 w-10 md:w-20 h-[2px] bg-zinc-500 transition-all duration-500 hover:w-full'></div>
           </div>
         </div>
-        <div className='w-full mt-28 h-[2px] bg-zinc-500 '></div>
+        <div className='w-full mt-20 md:mt-28 h-[2px] bg-zinc-500 '></div>
         {/* Services  */}
         {/* <OurServices /> */}
         <OurServices2/>
