17823ce3609b0b260df5600511e2fd9036ce62ad
Akshat Jaiswal <akshatjaiswal4841@gmail.com>
2025-02-06 23:04:59 +0530

    Refactor Page2: Add navigation links and adjust styling

--- src/components/Page2.jsx
+++ src/components/Page2.jsx
@@ -11,15 +11,15 @@ const Page2 = () => {
           <h1 className='font-bold font-[larken] text-2xl'>About Us</h1>
           <h1 className='mt-6 w-[23rem] font-[gilroy] text-base'>Welcome to WeInsightians, a Lucknow-based web development agency. We create stunning, high-performing websites for individuals, startups, and businesses of all sizes. Whether for a personal brand or a company profile, we bring your vision to life with tailored digital solutions. </h1>
           <div className='flex gap-2 sm:gap-2 mt-5'>
-            <button className='font-[gilroy] px-5 sm:px-8 md:px-10 py-1.5 sm:py-2 text-sm sm:text-base text-[#231746] font-semibold transition-transform-all duration-500 rounded-full border-2 bg-[#fff] hover:bg-[#534277] hover:text-white'>Learn More</button>
-            <div className='h-10 w-10 sm:h-12 sm:w-12 rounded-full flex transition-all duration-500 items-center justify-center p-1.5 sm:p-2 border-2 hover:rotate-45'><img src="/right-arrow-black.png" alt="" /></div>
+            <a href='/about' className='font-[gilroy] px-5 sm:px-8 md:px-10 py-1.5 sm:py-2 text-sm sm:text-base text-[#231746] font-semibold transition-transform-all duration-500 rounded-full border-2 bg-[#fff] hover:bg-[#534277] hover:text-white'>Learn More</a>
+            <a href='/about' className='h-10 w-10 sm:h-12 sm:w-12 rounded-full flex transition-all duration-500 items-center justify-center p-1.5 sm:p-2 border-2 hover:rotate-45'><img src="/right-arrow-black.png" alt="" /></a>
           </div>
         </div>
 
         {/* Left side Card  */}
         <div className="w-[90vw] sm:w-[40vw] mt-5">
           <div className="bg-custom h-[45vh] sm:h-[70vh] md:h-[80vh] rounded-2xl p-4">
-            <div className="bg-[#E3E7ED] h-full rounded-xl relative">
+            <div className="bg-[#ebebec] h-full rounded-xl relative">
               {/* Buttons */}
               <div className="absolute top-3 right-3 sm:top-6 sm:right-6 md:top-10 md:right-8 flex flex-col sm:flex-row gap-2 sm:space-x-2">
                 <button className="border-2 border-zinc-700 px-2 sm:px-3 md:px-4 py-1 sm:py-1.5 md:py-2 rounded-full text-xs sm:text-sm font-medium text-[#231746] transition duration-300 hover:bg-[#534277] hover:text-white">
@@ -42,37 +42,39 @@ const Page2 = () => {
                   Empowering Our Agency's Growth, and with great power comes great responsibility.
                 </h2>
                 <button className="mt-6 md:mt-20 px-5 md:px-8 py-2 md:py-4 border-zinc-700 border-2 text-xs sm:text-sm md:text-base hover:text-white font-semibold font-[gilroy] bg-transparent rounded-full transition duration-300 hover:bg-[#534277]">
+                  <a href="/contact" >
                   Get Started Now
+                </a>
                 </button>
               </div>
             </div>
           </div>
         </div>
-        </div>
+      </div>
 
-        {/* Right Side  */}
-        <div className='h-full w-full md:w-1/2'>
-          {/* Right Side Content */}
-          <div className='w-full h-1/3 pt-10'>
-            <div className='font-[larken] font-bold text-4xl sm:text-5xl md:text-6xl flex gap-5 justify-end'>
-              <h1>Transforming</h1>
-              <img className='h-10 sm:h-12 md:h-14' src="/circle-design.png" alt="" />
-            </div>
-            <div className='font-bold font-[larken] text-4xl sm:text-5xl flex justify-end'>
-              <h1>ideas into visually <br /> stunning realities.</h1>
-            </div>
+      {/* Right Side  */}
+      <div className='h-full w-full md:w-1/2'>
+        {/* Right Side Content */}
+        <div className='w-full h-1/3 pt-10'>
+          <div className='font-[larken] font-bold text-4xl sm:text-5xl md:text-6xl flex gap-5 justify-end'>
+            <h1>Transforming</h1>
+            <img className='h-10 sm:h-12 md:h-14' src="/circle-design.png" alt="" />
           </div>
-          {/* Right Side Service Content  */}
-          <div className='w-full mt-8 sm:mt-20 sm:h-full h-[calc(5*4rem)] overflow-x-auto flex flex-col gap-5'>
-            <ServiceCard title="Custom Websites 🡦" description="We build high-performance, user-friendly websites tailored to your business needs, ensuring a seamless experience, strong security, and smooth functionality across all devices." />
-            <ServiceCard title="E-Commerce 🡦" description="Launch and grow your online store with secure, scalable e-commerce platforms, complete with smooth navigation, payment gateways, and order management for an effortless shopping experience." />
-            <ServiceCard title="UI/UX Design 🡦" description="We create visually stunning, intuitive designs that enhance user experience, boost engagement, and reflect your brand’s identity, ensuring an easy and enjoyable journey for your visitors." />
-            <ServiceCard title="SEO & Marketing 🡦" description="Improve your online visibility with expert SEO and targeted digital marketing strategies that drive traffic, enhance search rankings, and connect you with the right audience." />
-            <ServiceCard title="Website Support 🡦" description="Keep your website secure, updated, and optimized with regular maintenance, performance enhancements, and technical support for a hassle-free online presence." />
+          <div className='font-bold font-[larken] text-4xl sm:text-5xl flex justify-end'>
+            <h1>ideas into visually <br /> stunning realities.</h1>
           </div>
+        </div>
+        {/* Right Side Service Content  */}
+        <div className='w-full mt-8 sm:mt-20 sm:h-full h-[calc(5*4rem)] overflow-x-auto flex flex-col gap-5'>
+          <ServiceCard title="Custom Websites 🡦" description="We build high-performance, user-friendly websites tailored to your business needs, ensuring a seamless experience, strong security, and smooth functionality across all devices." />
+          <ServiceCard title="E-Commerce 🡦" description="Launch and grow your online store with secure, scalable e-commerce platforms, complete with smooth navigation, payment gateways, and order management for an effortless shopping experience." />
+          <ServiceCard title="UI/UX Design 🡦" description="We create visually stunning, intuitive designs that enhance user experience, boost engagement, and reflect your brand’s identity, ensuring an easy and enjoyable journey for your visitors." />
+          <ServiceCard title="SEO & Marketing 🡦" description="Improve your online visibility with expert SEO and targeted digital marketing strategies that drive traffic, enhance search rankings, and connect you with the right audience." />
+          <ServiceCard title="Website Support 🡦" description="Keep your website secure, updated, and optimized with regular maintenance, performance enhancements, and technical support for a hassle-free online presence." />
         </div>
       </div>
-      )
+    </div>
+  )
 }
 
-      export default Page2
+export default Page2
