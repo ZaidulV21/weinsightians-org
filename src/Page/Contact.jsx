e160e3c0bb0944e65c2997adfce5098f1fe18837
Akshat Jaiswal <akshatjaiswal4841@gmail.com>
2025-02-06 23:07:30 +0530

    Update Contact Page: Refine contact details with interactive links and styling

--- src/Page/Contact.jsx
+++ src/Page/Contact.jsx
@@ -11,11 +11,11 @@ const Contact = () => {
       <Whatsapp />
       <Navbar />
       <div className='relative mt-2 md:mt-10 h-[70vh] md:h-[70vh] rounded-3xl overflow-hidden'>
-      <video className='h-full w-full object-cover' src="/video/moon.mp4" autoPlay loop muted></video>
+      <video className='h-full w-full object-cover brightness-[50%]' src="/video/moon.mp4" autoPlay loop muted></video>
         <div className=' absolute bottom-0 left-0 p-4'>
           <h1 className='text-4xl md:text-9xl uppercase font-[larken] font-bold mb-4 text-white'>Contact us</h1>
           <hr className='border-t border-white my-4' />
-          <p className='mb-8 text-sm md:text-2xl font-[gilroy] text-white'>Get in touch with us to  new people and explore new opportunities.Get in touch with us to discuss your project, ask questions, or simply say hello. We're always excited to meet new people and explore new opportunities..</p>
+          <p className='mb-8 text-sm md:text-2xl font-[gilroy] text-white'>We're here to help with anything you need. Want to get in touch with us? </p>
         </div>
       </div>
       <div className='mt-2 md:mt-10  flex h-full rounded-3xl'>
@@ -25,21 +25,23 @@ const Contact = () => {
           <div className='ml-14 font-[gilroy]'>
             <div>
               <img className='border-2 rounded-lg p-1 w-10 absolute left-16' src="/img/chat.png" alt="" />
-              <h1 className='mt-5 text-lg font-bold'>Chat to us</h1>
+              <h1 className='mt-5 text-lg font-bold'>Email us</h1>
               <h1 className=''>Our friendly team is here to help you</h1>
-              <h1 className='font-bold mt-3'>akshatjaiswal4841@gmail.com</h1>
+              <h1 className='font-bold mt-3 hover:text-gray-700 transition-all duration-500'>
+                <a href="mailto:mailtoweinsightians@gmail.com">mailtoweinsightians@gmail.com</a>
+              </h1>
             </div>
             <div>
               <img className='border-2 rounded-lg p-1 w-10 absolute left-16' src="/img/location.png" alt="" />
               <h1 className='mt-5 text-lg font-bold'>Visit us</h1>
               <h1 className=''>Come say hello to our office HQ.</h1>
-              <h1 className='font-bold mt-3'>Anirudh Nagar Kadipur </h1>
+              <a href="https://maps.app.goo.gl/TdXPwrSrYNBB1oKs5"><h1 className='font-bold mt-3 hover:text-gray-700 transition-all duration-500'> Matiyari Lucknow</h1></a>
             </div>
             <div>
               <img className='border-2 rounded-lg p-1 w-10 absolute left-16' src="/img/call.png" alt="" />
               <h1 className='mt-5 text-lg font-bold'>Talk to us</h1>
               <h1 className=''>Mon to Fri 8AM-9PM.</h1>
-              <h1 className='font-bold mt-3'>7309975088</h1>
+              <a href="tel:+917309975088" className='font-bold mt-3 hover:text-gray-700 transition-all duration-500'>+91 73099 75088</a>
             </div>
           </div>
           {/* social Links  */}
