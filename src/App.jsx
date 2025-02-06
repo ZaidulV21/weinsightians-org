988f3631e229561d0f955afd48caea9c26233068
Akshat Jaiswal <akshatjaiswal4841@gmail.com>
2025-02-06 23:04:07 +0530

    Add Privacy and Sitemap routes to App component

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Services from "./Page/Services"
 import About from "./Page/About"
 import Contact from "./Page/Contact"
 import Blog from "./Page/Blog"
+import Privacy from "./Page/privacy"
+import Sitemap from "./Page/Sitemap"
 
 
 export default function App() {
@@ -13,11 +15,13 @@ export default function App() {
     <div className="bg-white">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/services" element={<Services />} />
         <Route path="/about" element={<About />} />
         <Route path="/blog" element={<Blog />} />
-           <Route path="/blog" element={<Blog />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/privacy" element={<Privacy />} />
+        <Route path="/sitemap" element={<Sitemap />} />
       </Routes>
     
     </div>
