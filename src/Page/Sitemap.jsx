db207946f144e370d43ddf152ef5a345bc15644c
Akshat Jaiswal <akshatjaiswal4841@gmail.com>
2025-02-07 16:48:47 +0530

    Refactor Sitemap: Enhance layout with grid structure and add comprehensive navigation sections

--- src/Page/Sitemap.jsx
+++ src/Page/Sitemap.jsx
@@ -2,38 +2,64 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
-const sitemap = () => {
+const Sitemap = () => {
   return (
     <div className="h-full w-full text-black px-5 md:px-16 p-5 font-[gilroy]">
-      <Navbar/>
+      <Navbar />
       <div className="mt-10">
-        <h2 className="text-4xl md:text-7xl text-center font-bold mb-4 ">Sitemap</h2>
-        <ul className="list-none">
-          <li className="mb-4">
-            <a href="/" className='text-[#0F122E] text-lg'>Home</a>
-          </li>
-          <li className="mb-4">
-            <a href="/about" className='text-[#0F122E] text-lg'>About Us</a>
-          </li>
-          <li className="mb-4">
-            <a href="/services" className='text-[#0F122E] text-lg'>Services</a>
-          </li>
-          
-          <li className="mb-4">
-            <a href="/contact" className='text-[#0F122E] text-lg'>Contact Us</a>
-          </li>
-          <li className="mb-4">
-            <a href="/blog" className='text-[#0F122E] text-lg'>Blog</a>
-          </li>
-          <li className="mb-4">
-            <a href="/privacy" className='text-[#0F122E] text-lg'>Privacy Policy</a>
-          </li>
-        </ul>
+        <h2 className="text-4xl text-[#6B50A2] md:text-7xl font-bold mb-4">Sitemap</h2>
+        <div className="grid mt-10 grid-cols-2 md:grid-cols-4 gap-10">
+          <div>
+            <h3 className="font-bold text-2xl">Main</h3>
+            <ul className="leading-10 text-lg">
+              <li><a href="/">Home</a></li>
+              <li><a href="/services">Services</a></li>
+              <li><a href="/blog">Blog</a></li>
+              <li><a href="#">Designer</a></li>
+              
+              <li><a href="#">Ecommerce</a></li>
+              
+              <li><a href="#">SEO</a></li>
+              <li><a href="/about">About</a></li>
+            </ul>
+          </div>
+          <div>
+            <h3 className="font-bold text-2xl">Privacy</h3>
+            <ul className="leading-10 text-lg">
+              <li><a href="/privacy">Privacy Policy</a></li>
+              <li><a href="https://youtube.com" target='_blank'>Live Stream Archive</a></li>
+            </ul>
+          </div>
+          <div>
+            <h3 className="font-bold text-2xl">Community</h3>
+            <ul className="leading-10 text-lg">
+              <li><a href="#">Webflow Community</a></li>
+              <li><a href="#">Forum</a></li>
+              <li><a href="#">Discover</a></li>
+              <li><a href="#">Webflow Designers</a></li>
+            </ul>
+          </div>
+          <div>
+            <h3 className="font-bold text-2xl">Resources</h3>
+            <ul className="leading-10 text-lg">
+              <li><a href="https://dribbble.com/" target='_blank'>Help Site</a></li>
+              <li><a href="#"></a></li>
+              <li><a href="#">Ebooks</a></li>
+              <li><a href="#">Service Status</a></li>
+              <li><a href="">Website Builder</a></li>
+              <li><a href="#">Website Design Software</a></li>
+              <li><a href="#">Responsive Website Builder</a></li>
+              <li><a href="#">CMS Website</a></li>
+              <li><a href="#">Website Creator</a></li>
+              <li><a href="#">Website Templates</a></li>
+            </ul>
+          </div>
+        </div>
       </div>
-      <Footer/>
+      <Footer />
     </div>
   );
 };
 
-export default sitemap;
+export default Sitemap;
 
